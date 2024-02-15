6acd0c70b95b89342ce6cee60657f44f0f22ec79
cescanue <cescanuela@gmail.com>
2024-02-15 23:11:07 +0100

    remove deisconnected users

--- srcs/commands/quit.cpp
+++ srcs/commands/quit.cpp
@@ -6,7 +6,7 @@
 /*   By: cescanue <cescanue@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 23:07:01 by cescanue          #+#    #+#             */
-/*   Updated: 2024/02/15 23:07:03 by cescanue         ###   ########.fr       */
+/*   Updated: 2024/02/15 23:10:50 by cescanue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,10 +42,10 @@ void IRCCore::quit(IRCClient &client, std::string message)
 		}
 		for (mapClients::iterator it = _clients.begin(); it != _clients.end(); it++)
 		{
-			if (it->second.getNickname() == client.getNickname())
+			if (client.getClientAuthentication() && it->second.getNickname() == client.getNickname())
 			{
 				close(it->first);
-				//_clients.erase(it->first);		// this deletion provokes a segfault
+				//_clients.erase(it);		// this deletion provokes a segfault
 				break;
 			}
 		}
